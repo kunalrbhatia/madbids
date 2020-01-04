dea68e9843182c0917f2c40c0933ad8c37dc7e3f
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-01-04 13:18:19 +0530

    login created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import Login from "./components/users/login";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./components/common/history";
+import { Helper } from "./components/common/helper";
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+    this.Helper = new Helper();
+    this.state = {
+      globalVars: {
+        //base_URL: "http://35.200.211.35/dbk/",
+        //base_URL: "http://192.168.0.20:3100/api/v1/", // vaibhav
+        //base_URL: "http://192.168.0.24:3100/api/v1/", // vaibhav
+        base_URL: "http://localhost:3100/api/v1/", // local
+        //base_URL: "http://192.168.0.25:3100/api/v1/", // Kunal
+        userId: "1"
+      }
+    };
+  }
+  render() {
+    let hash = history.location.pathname;
+    const { globalVars } = this.state;
+    if (hash === "/login" || hash === "/signup") {
+      return (
+        <Router basename={"/db"} history={history}>
+          <Route path="/login" component={props => <Login {...props} globalVars={globalVars} />} />
+          {/* <Route path="/signup" component={props => <Signup {...props} globalVars={globalVars} />} /> */}
+        </Router>
+      );
+    } else {
+      return (
+        <Router basename={"/db"} history={history}>
+          <Route path="*" component={props => <Login {...props} globalVars={globalVars} />} />
+        </Router>
+      );
+    }
+  }
+  /* return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -15,7 +49,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  ); */
 }
 
 export default App;
---
+++ src/components/common/FormElements.js
@@ -0,0 +1,327 @@
+import React from "react";
+import clsx from "clsx";
+/* import PropTypes from "prop-types";
+import NoSsr from "@material-ui/core/NoSsr";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Chip from "@material-ui/core/Chip";
+import CancelIcon from "@material-ui/icons/Cancel"; */
+import "date-fns";
+import Checkbox from "@material-ui/core/Checkbox";
+import DateFnsUtils from "@date-io/date-fns";
+import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
+import { makeStyles, emphasize /* useTheme */ } from "@material-ui/core/styles";
+/* import "react-inputs-validation/lib/react-inputs-validation.min.css"; */
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Switch from "@material-ui/core/Switch";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { IconButton, Snackbar/* , Icon  */} from "@material-ui/core/";
+import CloseIcon from "@material-ui/icons/Close";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+//import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1)
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500]
+  },
+  button: {
+    margin: theme.spacing(1),
+    color: "white"
+  },
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    fontSize:12,
+    "& label.Mui-focused": {
+      color: "#2AAA9E"
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#2AAA9E"
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "1px solid rgba(224, 224, 224, 1)"
+      },
+      "& fieldset legend": {
+        width: 200,
+        borderColor: "#2AAA9E"
+      },
+      "&:hover fieldset": {
+        borderColor: "#00A0DB"
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#2AAA9E"
+      }
+    } /* ,
+    '& input:invalid + fieldset': {
+      borderColor: 'red',
+      borderWidth: 2,
+    },
+    '& input:invalid +fieldset legend': {
+      color: 'red'
+    } */
+  },
+  dense: {
+    marginTop: theme.spacing(1),
+    marginLeft: theme.spacing(0)
+  },
+  menu: {
+    width: 200
+  },
+  valueContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    flex: 1,
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main
+  },
+  chip: {
+    margin: theme.spacing(0.5, 0.25)
+  },
+  chipFocused: {
+    backgroundColor: emphasize(theme.palette.type === "light" ? theme.palette.grey[300] : theme.palette.grey[700], 0.08)
+  },
+  noOptionsMessage: {
+    padding: theme.spacing(1, 2)
+  },
+  singleValue: {
+    fontSize: 16
+  },
+  placeholder: {
+    position: "absolute",
+    left: 2,
+    bottom: 6,
+    fontSize: 16
+  },
+  paper: {
+    position: "absolute",
+    zIndex: 1,
+    marginTop: theme.spacing(1),
+    left: 0,
+    right: 0
+  }
+}));
+
+function MDialog(params) {
+  const classes = useStyles();
+  let buttonHTML = function(buttons) {
+    return buttons.map(button => {
+      return (
+        <MButton onClick={params.close} key={button.value} color={button.color}>
+          {button.value}
+        </MButton>
+      );
+    });
+  };
+  return (
+    <Dialog
+      open={params.open}
+      onClose={params.close}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title" disableTypography className={classes.root}>
+        {params.title}
+        <IconButton aria-label="close" className={classes.closeButton} onClick={params.close}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{params.children}</DialogContentText>
+      </DialogContent>
+
+      <DialogActions>{buttonHTML(params.buttons)}</DialogActions>
+    </Dialog>
+  );
+}
+function MButton(params) {
+  const classes = useStyles();
+  return (
+    <Button
+      disabled={params.disabled ? params.disabled : false}
+      fullWidth={params.fullWidth}
+      variant={params.variant ? params.variant : "contained"}
+      color={params.color ? params.color : "primary"}
+      name={params.name}
+      className={classes.button}
+      onClick={params.onClick}
+      startIcon={params.icon}
+    >
+      {params.children}
+    </Button>
+  );
+}
+
+function MSwithch(params) {
+  return (
+    <FormControlLabel
+      control={<Switch name={params.name} id={params.id} checked={params.isSelected} onChange={params.onChange} />}
+      value={params.value}
+      label={params.label}
+      labelPlacement={params.labelPlacement ? params.labelPlacement : "start"}
+    />
+  );
+}
+
+function MCheckbox(params) {
+  return (
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Checkbox
+            name={params.name}
+            id={params.id}
+            color={"primary"}
+            checked={params.value}
+            onChange={params.onChange}
+            value={params.name}
+          />
+        }
+        label={params.label}
+      />
+    </FormGroup>
+  );
+}
+
+function MTextField(params) {
+  const classes = useStyles();
+  if (params.type === "select") {
+    return (
+      <TextField
+        key={params.name}
+        id={params.name}
+        select
+        label={params.label}
+        disabled={params.disabled}
+        fullWidth={params.fullWidth ? params.fullWidth : false}
+        margin="dense"
+        onChange={params.onChange}
+        className={clsx(classes.textField, classes.dense)}
+        value={params.value}
+        name={params.name}
+        SelectProps={{
+          MenuProps: {
+            className: classes.menu
+          }
+        }}
+        helperText={params.helperText}
+        variant="outlined"
+      >
+        {params.data.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    );
+  } else if (
+    params.type === "text" ||
+    params.type === "number" ||
+    params.type === "email" ||
+    params.type === "password"
+  ) {
+    return (
+      <TextField
+        id={params.id}
+        disabled={params.disabled ? params.disabled : false}
+        required={params.required ? params.required : false}
+        error={params.error ? params.error : false}
+        multiline={params.multiline ? params.multiline : false}
+        type={params.type}
+        autoFocus={params.autoFocus ? params.autoFocus : false}
+        name={params.name}
+        fullWidth={params.fullWidth ? params.fullWidth : false}
+        label={"" + params.label}
+        className={clsx(classes.textField, classes.dense)}
+        margin="dense"
+        rows={params.rows ? params.rows : 1}
+        onChange={params.onChange}
+        value={params.value}
+        variant={params.variant ? params.variant : "outlined"}
+        helperText={params.helperText}
+      />
+    );
+  } else if (params.type === "date") {
+    return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <DatePicker
+          disableToolbar
+          variant="inline"
+          inputVariant="outlined"
+          autoOk={true}
+          disabled={params.disabled ? params.disabled : false}
+          helperText={params.helperText}
+          fullWidth={params.fullWidth ? params.fullWidth : false}
+          format="dd/MM/yyyy"
+          margin="dense"
+          className={clsx(classes.textField, classes.dense)}
+          id={params.id ? params.id : params.name}
+          allowKeyboardControl={true}
+          label={params.label}
+          value={params.value}
+          onChange={params.onChange}
+          onFocus={params.onFocus}
+        />
+      </MuiPickersUtilsProvider>
+    );
+  }
+}
+
+function MSnackbar(params) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(params.open);
+
+  const handleClose = (e, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: params.vPos ? params.vPos : "bottom",
+        horizontal: params.hPos ? params.hPos : "left"
+      }}
+      open={open}
+      autoHideDuration={params.autoHideDuration ? params.autoHideDuration : 600000}
+      onClose={handleClose}
+      className={classes.info}
+      ContentProps={{
+        "aria-describedby": "message-id"
+      }}
+      message={params.message}
+      action={[
+        <IconButton key="close" aria-label="close" color="inherit" className={classes.close} onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      ]}
+    />
+  );
+}
+
+export { MTextField, MButton, MDialog, MSwithch, MCheckbox, MSnackbar };
