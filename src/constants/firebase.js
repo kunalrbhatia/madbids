dcd49f7f5858ccecb062b1c23204f2eef59caf21
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-01-08 12:24:36 +0530

    added firebase

---
+++ src/constants/firebase.js
@@ -0,0 +1,12 @@
+export const firebase = {
+  dev: {
+    apiKey: "AIzaSyBxWAv-UhRRx2_1etDDSo4b_ot9U046pvM",
+    authDomain: "madbids-e13ee.firebaseapp.com",
+    databaseURL: "https://madbids-e13ee.firebaseio.com",
+    projectId: "madbids-e13ee",
+    storageBucket: "madbids-e13ee.appspot.com",
+    messagingSenderId: "23154373829",
+    appId: "1:23154373829:web:91de7aaedafb3b2c"
+  },
+  prod: {}
+};
