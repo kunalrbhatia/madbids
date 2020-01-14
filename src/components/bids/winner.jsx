3cc4e08aa20146ccf2b5393dc8f54d7f8bb532ac
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-01-14 18:25:17 +0530

    apis work

--- src/components/bids/winner.jsx
+++ src/components/bids/winner.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Container, Box } from "@material-ui/core";
 import { Copyright } from "../common/FormElements";
 //import * as ROUTES from "../../constants/routes";
+import * as APIS from "../../constants/fbapis";
 import { withFirebase } from "../Firebase";
 import { compose } from "recompose";
 class Winner extends Component {
@@ -10,10 +11,57 @@ class Winner extends Component {
     this.state = {
       onChange: this.handleChange(),
       bidlist: [],
-      userslist: []
+      userslist: [],
+      apis: [
+        { name: APIS.AUCTIONS, data: [], url: this.props.firebase.auctions() },
+        { name: APIS.BIDLIST, data: [], url: this.props.firebase.bids() },
+        { name: APIS.USERS, data: [], url: this.props.firebase.users() }
+      ]
     };
-    this.getValuesFromDB();
+    this.helper = this.props.helper;
+    this.current = 0;
+    this.total = this.state.apis.length;
+    this.getDataFromDB();
+    //this.getValuesFromDB();
   }
+  getDataFromDB = () => {
+    if (this.current < this.total) {
+      if (!this.props.globalVars["" + this.state.apis[this.current].name]) {
+        this.state.apis[this.current].url.on("value", snapshot => {
+          const object = snapshot.val();
+          const _list = Object.keys(object).map(key => ({
+            ...object[key],
+            id: key
+          }));
+          let apis_copy = this.state.apis;
+          apis_copy[this.current]["data"] = _list;
+          this.setState(
+            {
+              apis: apis_copy
+            },
+            () => {
+              this.current++;
+              this.getDataFromDB();
+            }
+          );
+        });
+      } else {
+        let apis_copy = this.state.apis;
+        apis_copy[this.current]["data"] = this.props.globalVars["" + this.state.apis[this.current].name];
+        this.setState(
+          {
+            apis: apis_copy
+          },
+          () => {
+            this.current++;
+            this.getDataFromDB();
+          }
+        );
+      }
+    } else {
+      console.log(this.state);
+    }
+  };
   getValuesFromDB = () => {
     this.props.firebase.bids().on("value", async snapshot => {
       const bidsObject = snapshot.val();
--- src/constants/fbapis.js
+++ src/constants/fbapis.js
@@ -1,3 +1,5 @@
 export const PRODUCTS = "products";
 export const AUCTIONS = "auctions";
 export const USERS = "users";
+export const BIDLIST = "bidlist";
+export const BID = "bid";
