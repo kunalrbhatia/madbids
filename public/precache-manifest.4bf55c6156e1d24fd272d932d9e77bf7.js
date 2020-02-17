cfd2b1b0c417e0d728a63821fc671f0d6063aa89
kunalrbhatia <kunal.bhatia@dailybooks.in>
2020-02-17 11:36:35 +0530

    nav working on android

---
+++ public/precache-manifest.4bf55c6156e1d24fd272d932d9e77bf7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff9b1e9b30918af2f7db9fa986a6f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b41c5379a2751bdc7de",
+    "url": "/static/css/main.a3dc3594.chunk.css"
+  },
+  {
+    "revision": "91653ab83dd3980b95e2",
+    "url": "/static/js/2.0d8c9aec.chunk.js"
+  },
+  {
+    "revision": "48a7f982264b141da44db572a26ecb94",
+    "url": "/static/js/2.0d8c9aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b41c5379a2751bdc7de",
+    "url": "/static/js/main.8ca2bd5f.chunk.js"
+  },
+  {
+    "revision": "e89e4ee769c7dcd32b8f",
+    "url": "/static/js/runtime-main.e2153e8c.js"
+  },
+  {
+    "revision": "0209214b6f95f1db61361764124e5748",
+    "url": "/static/media/noInternet.0209214b.png"
+  },
+  {
+    "revision": "0c5381bec54297c55a3d2ccd53d390b0",
+    "url": "/static/media/winner.0c5381be.jpg"
+  }
+]);
